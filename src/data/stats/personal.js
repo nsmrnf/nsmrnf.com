032739e851889660b0c097534f89d13a2c9f153d
Nikita Smirnov <nick.smirnov2015@gmail.com>
2022-10-01 17:09:09 +0300

    stats

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -5,7 +5,7 @@ const Age = () => {
 
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
+    const birthTime = new Date('1997-12-22T09:24:00');
     setAge(((Date.now() - birthTime) / divisor).toFixed(11));
   };
 
@@ -27,14 +27,14 @@ const data = [
   {
     key: 'countries',
     label: 'Countries visited',
-    value: 53,
+    value: 17,
     link:
-      'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
+      'https://www.google.com/maps/d/embed?mid=1c5zMzPBFAJTJbDs20372YWo7KOouezE&ehbc=2E312F',
   },
   {
     key: 'location',
     label: 'Current city',
-    value: 'New York, NY',
+    value: 'Minsk, Belarus',
   },
 ];
 
