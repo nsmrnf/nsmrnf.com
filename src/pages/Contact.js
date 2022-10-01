858e7cdf0f55cec1cf51deaaa99a3a30c0905c2c
Nikita Smirnov <nick.smirnov2015@gmail.com>
2022-10-01 14:11:49 +0300

    1

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ michael.l.dangelo@gmail.com"
+    description="Contact Nikita Smirnov via email @ me@nsmrnf.com"
   >
     <article className="post" id="contact">
       <header>
